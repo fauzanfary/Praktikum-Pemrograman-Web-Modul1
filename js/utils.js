0022e89077dc100414b918226d8c9279a6f1672b
FauzanFary <algif776@gmail.com>
2026-09-16 21:10:32 +0800

    sukses

--- js/utils.js
+++ js/utils.js
@@ -13,17 +13,29 @@ export const formatRupiah = (angka) => {
 
 /**
  * Mendapatkan konfigurasi UI berdasarkan status harga
+ * Dalam harga pangan: Harga Naik = Merah (Warning), Harga Turun = Hijau (Good)
  */
 export const getBadgeConfig = (status, selisih) => {
     if (status === 'up') {
-        return { class: 'up', icon: 'fa-arrow-up', text: `Harga Naik Rp ${selisih}` };
+        return { 
+            class: 'up', 
+            icon: 'fa-arrow-up', 
+            text: `Harga Naik Rp ${selisih}` 
+        };
     } else if (status === 'down') {
-        return { class: 'down', icon: 'fa-arrow-down', text: `Harga Turun Rp ${selisih}` };
+        return { 
+            class: 'down', 
+            icon: 'fa-arrow-down', 
+            text: `Harga Turun Rp ${selisih}` 
+        };
     } else {
-        return { class: 'stable', icon: 'fa-minus', text: 'Stabil' };
+        return { 
+            class: 'stable', 
+            icon: 'fa-minus', 
+            text: 'Stabil' 
+        };
     }
-};
-
+}
 /**
  * FUNGSI PRAKTIKUM MODUL 4: Mengolah ringkasan data komoditas
  * Menggunakan Arrow Function, array method (reduce, filter), dan Error Handling
